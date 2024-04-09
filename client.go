1215b6b6ab1d026e609fe425c5c9b767ee8993bb
uchitnm <uchitnm@gmail.com>
2024-04-09 16:18:00 +0530

    Keyboard intrept handler

--- client.go
+++ client.go
@@ -6,34 +6,36 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
 	str "strings"
+	"syscall"
 )
 
-type Message struct{
-	Msg string `json:"Msg"`
+type Message struct {
+	Msg  string `json:"Msg"`
 	Info string `json:"Info"`
 }
 
 func Serialize(msg Message) []byte {
-	b, err:=json.Marshal(msg)
-	if (err!=nil){
+	b, err := json.Marshal(msg)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return b
 }
 
-func DeSerialize(obj []byte) Message{
-	var msg=Message{}
-	err:=json.Unmarshal(obj, &msg)
-	if (err!=nil){
+func DeSerialize(obj []byte) Message {
+	var msg = Message{}
+	err := json.Unmarshal(obj, &msg)
+	if err != nil {
 		fmt.Println(err)
 	}
 	return msg
-} 
+}
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:5000")
 
+	var conn, err = net.Dial("tcp", "localhost:5000")
 	if err != nil {
 		fmt.Println("Error connecting to server:", err)
 		return
@@ -44,7 +46,7 @@ func main() {
 
 	name := ""
 	fmt.Print("Enter your name: ")
-	fmt.Scanln(&name) 
+	fmt.Scanln(&name)
 	recipient := ""
 	fmt.Print("Enter the Recipient's Name: ")
 	fmt.Scanln(&recipient)
@@ -60,18 +62,18 @@ func main() {
 
 	go func() {
 		for {
-			var msg Message=Message{}
+			var msg Message = Message{}
 			buf := make([]byte, 2048)
 			recv_len, err := conn.Read(buf)
 			if err != nil {
 				fmt.Println("Error decoding message:", err)
 			}
-			msg=DeSerialize(buf[:recv_len])
+			msg = DeSerialize(buf[:recv_len])
 			if msg.Info == "CLIENT_NOT_CONN" {
 				fmt.Println("Recipient client not connected.")
-			}else if msg.Info=="SUCCESS"{
+			} else if msg.Info == "SUCCESS" {
 				continue
-			}else{
+			} else {
 				fmt.Printf("\b\b\b\b\b\b")
 				fmt.Printf("%s : %s\n", recipient, msg.Msg)
 				fmt.Printf("You : ")
@@ -79,17 +81,38 @@ func main() {
 		}
 	}()
 
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("\nExiting...")
+		conn.Write(Serialize(Message{Msg: "", Info: "CLOSE"}))
+		os.Exit(1)
+	}()
+
 	for {
-		reader:=bufio.NewReader(os.Stdin)
+		reader := bufio.NewReader(os.Stdin)
 		fmt.Printf("You : ")
-		var message string=""
+		var message string = ""
 		message, _ = reader.ReadString('\n')
-		message=str.Trim(message, "\n")
+		message = str.Trim(message, "\n")
 
 		if message == "exit" {
-			conn.Write(Serialize(Message{Msg:"", Info: "CLOSE"}))
+			conn.Write(Serialize(Message{Msg: "", Info: "CLOSE"}))
 			break
 		}
-		conn.Write(Serialize(Message{Msg:message, Info:""}))
+		conn.Write(Serialize(Message{Msg: message, Info: ""}))
 	}
+
 }
+
+// func init() {
+//     c := make(chan os.Signal)
+//     signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+//     go func() {
+//         <-c
+//         fmt.Println("\nExiting...")
+// 		conn.Write(Serialize(Message{Msg:"", Info: "CLOSE"}))
+//         os.Exit(1)
+//     }()
+// }
